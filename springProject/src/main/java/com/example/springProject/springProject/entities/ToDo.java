f02e471bc6a839002296b337402dba3791103b37
mofachhalhossain <78526884+mofachhalhossain@users.noreply.github.com>
2021-03-07 13:53:54 +0600

    Defined Entities.Fetched in database & defined relation.

---
+++ springProject/src/main/java/com/example/springProject/springProject/entities/ToDo.java
@@ -0,0 +1,74 @@
+package com.example.springProject.springProject.entities;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity                                               //0.0.006->defines entity
+@Table(name="Todo")                                   //0.0.007->for changing tables in database
+public class ToDo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)     //0.0.008->for changing tables in database
+    private int id;
+    private String time;
+    private String work;
+    @Column(length = 180)                                //0.0.009->for changing field
+    private String description;
+    private String imageurl;
+
+    public ToDo() {
+        super();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getWork() {
+        return work;
+    }
+
+    public void setWork(String work) {
+        this.work = work;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImageurl() {
+        return imageurl;
+    }
+
+    public void setImageurl(String imageurl) {
+        this.imageurl = imageurl;
+    }
+
+
+    @ManyToOne
+    private User user;
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
---
+++ springProject/src/main/java/com/example/springProject/springProject/entities/User.java
@@ -0,0 +1,98 @@
+package com.example.springProject.springProject.entities;
+
+
+import org.springframework.data.util.Lazy;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+@Entity                                   //0.0.001->defines entity
+@Table(name ="User")                      //0.0.002->for changing tables in database
+
+
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)        //0.0.003->for generating auto value
+    private int id;
+
+    @Column(length = 20)                       //0.0.004->for changing field
+    private String name;
+    @Column(unique = true)                     //0.0.005->changing field
+    private String email;
+    private String password;
+    private String role;
+    private String imageUrl;
+
+
+
+    public User() {
+        super();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY)         // for specifing database relation // CascadeType.ALL will help like when user will be deleted do will delete automatic
+    //visit: https://howtodoinjava.com/hibernate/how-to-define-association-mappings-between-hibernate-entities/
+
+    public List<ToDo> todo;
+    {
+        todo = new ArrayList<>();
+    }
+
+    public List<ToDo> getTodo() {
+        return todo;
+    }
+
+    public void setTodo(List<ToDo> todo) {
+        this.todo = todo;
+    }
+}
+
