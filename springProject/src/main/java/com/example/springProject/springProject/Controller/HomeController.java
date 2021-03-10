e62909a03f27cda8b34a4aed2cf0eabb40795d7a
mofachhal hossain <mofachhlahossain101@gmail.com>
2021-03-10 17:03:32 +0600

    controller implemented for home and about

--- springProject/src/main/java/com/example/springProject/springProject/Controller/HomeController.java
+++ springProject/src/main/java/com/example/springProject/springProject/Controller/HomeController.java
@@ -4,13 +4,38 @@ import com.example.springProject.springProject.Dao.UserRepository;
 import com.example.springProject.springProject.entities.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 public class HomeController {
 
-    @Autowired
-    private UserRepository userRepository;
+//    @Autowired
+//    private UserRepository userRepository;
+//
+//    @GetMapping("/test")
+//    @ResponseBody
+//    public String test(){
+//        User user = new User();
+//        user.setName("Noname");
+//        user.setRole("admin");
+//        userRepository.save(user);
+//        return "working";
+//    }
+
+    @RequestMapping("/home")
+    public String home(Model model){
+        model.addAttribute("title","Home-ToDo");
+        return "home";
+    }
+
+    @RequestMapping("/about")
+    public String about(Model model){
+        model.addAttribute("title","About-ToDo");
+        return "about";
+    }
 
 }
